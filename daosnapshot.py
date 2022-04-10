ff6d3f636458cffcfe60784faa9f2f0343321945
roshgregory <roshangnoronha@gmail.com>
2022-04-10 13:33:41 +0530

    API

--- daosnapshot.py
+++ daosnapshot.py
@@ -1,14 +1,25 @@
 from flask_restful import Resource
+from tracemalloc import start
 import requests
 import json
 import pandas as pd
+import time
+
 import os
+etherscan_api = os.getenv('etherscan_api')
 from dotenv import load_dotenv
+import os
+load_dotenv()
+os.getenv('etherscan_api')
+# get current time in seconds
+t = time.time()
+end_block = int(t)
+start_block=end_block-60*60*24*30
 
 link = "https://hub.snapshot.org/graphql"
 
 ens_name = ""
-
+ret_data={}
 class DAO(Resource):
     def get(self,n):
         ens_name = n
@@ -21,10 +32,14 @@ class DAO(Resource):
                         }
                     }"""
         r = requests.post(link, json={'query': query}).json()
-        if r['data']['space']['strategies'][0]['params']['strategies'][0]['name']=='erc20-votes':
+
+        #Work on Strategy Code
+        '''if r['data']['space']['strategies'][0]['params']['strategies'][0]['name']=='erc20-votes':
             print('Strategy support - ERC20')
         else:
-            exit()
+            exit()'''
+
+
         query  = """query {
                     proposals(
                           first:100000,
@@ -34,6 +49,7 @@ class DAO(Resource):
                         )
                         {
                             id
+                            created
                         }
 
                         
@@ -42,10 +58,18 @@ class DAO(Resource):
         list_of_proposals = []
         for id in r["data"]["proposals"]:
             list_of_proposals.append(id["id"])
+
+        prop_count=0
+        for proposal in r["data"]["proposals"]:
+            if proposal['created']>start_block:
+                prop_count+=1
+        
+        ret_data['Num of Proposals'] = prop_count
+                
         list_str = str(list_of_proposals).replace("'","\"")
         query = """query {
                     votes (
-                        first: 10000
+                        first: 100000000                       
 
                         where: {
                         proposal_in: """ + list_str + """
@@ -53,23 +77,33 @@ class DAO(Resource):
                         orderBy: "created",
                         orderDirection: desc
                     ) {
-                        
+                        created
                         voter
 
                     }
                     }"""
-        etherscan_api = os.getenv('etherscan_api')
-        os.getenv('etherscan_api')
-        link_block = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey="+os.getenv('etherscan_api')
-        end_block=int(requests.get(link_block).json()['result'],16)
-        start_block=end_block-200000
+        
 
         r = requests.post(link, json={'query': query}).json()
+
+                
+        voter_list=[]
+        for voter in r['data']['votes']:
+            voter_list.append(voter['voter'])
+
+        print(len(list(set(voter_list))))
         user_dict={}
         for voter in r['data']['votes']:
-            if voter['voter'] not in user_dict:
+            if voter['voter'] not in user_dict and voter['created']>start_block:
                 user_dict[voter['voter']] = 1
-            else:
+            elif voter['created']>start_block:
                 user_dict[voter['voter']] += 1
-        json_object = json.dumps(user_dict, indent = 4)
+                
+                    
+       
+        ret_data['vote_count'] = len(list(set(user_dict.keys())))
+        ret_data['max_vote']= max(user_dict.values())
+
+        ret_data['votes']=user_dict
+        json_object = json.dumps(ret_data, indent = 4)
         return json_object,200
