58574fb62a00838b09b5bff1ebb60b0b34df7443
roshgregory <roshangnoronha@gmail.com>
2022-04-10 12:38:34 +0530

    data edits

--- API/data.py
+++ API/data.py
@@ -1,6 +1,16 @@
+from tracemalloc import start
 import requests
 import json
 import pandas as pd
+import time
+
+import os
+etherscan_api = os.getenv('etherscan_api')
+from dotenv import load_dotenv
+import os
+load_dotenv()
+os.getenv('etherscan_api')
+#print(os.getenv('etherscan_api'))
 
 ens_name = "ens.eth"
 
@@ -27,7 +37,7 @@ if r['data']['space']['strategies'][0]['params']['strategies'][0]['name']=='erc2
 else:
     exit()
 
-#2. Get user Base
+#2. Get Proposal list Base
 
 link = "https://hub.snapshot.org/graphql"
 query  = """query {
@@ -48,11 +58,15 @@ query  = """query {
 
 r = requests.post(link, json={'query': query}).json()
 list_of_proposals = []
+print(len(list_of_proposals))
 for id in r["data"]["proposals"]:
     list_of_proposals.append(id["id"])
-
+print(len(list_of_proposals))
 list_str = str(list_of_proposals).replace("'","\"")
 
+
+
+#Get Voters list of entire DAO 
 query = """query {
 votes (
     first: 10000
@@ -63,36 +77,38 @@ votes (
     orderBy: "created",
     orderDirection: desc
 ) {
-    
+    created
     voter
 
 }
 }"""
 
-
-#Get Block Size for past Month
-import os
-etherscan_api = os.getenv('etherscan_api')
-from dotenv import load_dotenv
-import os
-load_dotenv()
-os.getenv('etherscan_api')
-#print(os.getenv('etherscan_api'))
-link_block = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey="+os.getenv('etherscan_api')
-print(requests.get(link_block).json())
-
-end_block=int(requests.get(link_block).json()['result'],16)
-start_block=end_block-200000
-
-print(start_block,end_block)
 r = requests.post(link, json={'query': query}).json()
+
+voter_list=[]
+for voter in r['data']['votes']:
+  voter_list.append(voter['voter'])
+
+print(len(list(set(voter_list))))
+
+'''
+
+link_block = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey="+os.getenv('etherscan_api')
+
+end_block=int(requests.get(link_block).json()['result'],16)'''
+
+
+# get current time in seconds
+t = time.time()
+end_block = int(t)
+start_block=end_block-60*60*24*30
+
 user_dict={}
 for voter in r['data']['votes']:
-    if voter['voter'] not in user_dict:
+    if voter['voter'] not in user_dict and voter['created']>start_block:
         user_dict[voter['voter']] = 1
-    else:
+    elif voter['created']>start_block:
         user_dict[voter['voter']] += 1
-#print(user_dict)
 
 print(len(user_dict))
 
