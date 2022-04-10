4a60d91a30e8fff157e7f02219921782485403a8
roshgregory <roshangnoronha@gmail.com>
2022-04-10 17:08:54 +0530

    dta add

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
+
 from flask import Flask
-from flask.ext.cors import CORS, cross_origin
 from flask_restful import Api
 from flask_cors import CORS
 
 from daosnapshot import DAO
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 api.add_resource(DAO, "/dao/<string:n>")
 
-
 if __name__ == "__main__":
   app.run()
-
